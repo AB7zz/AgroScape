b54dccd04273d6995a31a54c89e8562f2d6454e8
Jishnu M M <jishnumm4312@gmail.com>
2024-02-16 20:19:01 +0530

    added database connectivity

--- packages/server/config/db.config.js
+++ packages/server/config/db.config.js
@@ -7,7 +7,7 @@ const username = process.env.MONGO_USERNAME
 const password = process.env.MONGO_PASSWORD
 
 const Connection = async () => {
-    const URL = `mongodb+srv://${username}:${password}@blinkit.pid1ulu.mongodb.net/?retryWrites=true&w=majority`
+    const URL = `mongodb+srv://${username}:${password}@cluster0.jy7pzib.mongodb.net/?retryWrites=true&w=majority`
     try{
         await mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
         console.log('Database connected successfully')
