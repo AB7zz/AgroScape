ff4023414765f272e8f44d63834ac7f11ed8635c
AB7zz <abhinavcv007@gmail.com>
2024-02-16 19:20:20 +0530

    frontend folder structure completed

---
+++ packages/client/src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MarketPlace from './pages/MarketPlace'
+import Game from './pages/Game'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Forums from './pages/Forums'
+import Forum from './pages/Forum'
+import Checkout from './pages/Checkout'
+import Cart from './pages/Cart'
+import News from './pages/News'
+import NewsArticle from './pages/NewsArticle'
+import Orders from './pages/Orders'
+import Home from './pages/Home'
+import Steps from './pages/Steps'
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/marketplace" element={<MarketPlace />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="*">"Not Found"</Route>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forums" element={<Forums />} />
+          <Route path="/forum/:id" element={<Forum />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/news/:id" element={<NewsArticle />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/steps" element={<Steps />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
