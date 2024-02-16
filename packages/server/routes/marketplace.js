1b4b9d588b5bf2864beb668c0ba24dd14b1a927d
Jishnu M M <jishnumm4312@gmail.com>
2024-02-17 00:44:13 +0530

    added fetch.js in marketplace

--- packages/server/routes/marketplace.js
+++ packages/server/routes/marketplace.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import Insertmarketplace from '../controllers/marketplace/index.js'
+import Fetchmarketplace from '../controllers/marketplace/fetch.js'
 const router = express.Router()
 
 router.post('/marketplace', Insertmarketplace)
+router.get('/marketplace', Fetchmarketplace)
 
 export default router
